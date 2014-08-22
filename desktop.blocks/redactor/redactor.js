da27d64bcaaad4587920deb5da679fa975a60524
voischev <voischev.ivan@ya.ru>
2014-08-22 16:49:18 +0400

    ym redactor start

--- desktop.blocks/redactor/redactor.js
+++ desktop.blocks/redactor/redactor.js
@@ -1,12 +1,40 @@
+//TODO: Завернуть плагины в модульную систему.
+
 /*borschik:include:redactor.min.js*/;
 /*borschik:include:ru.js*/;
 /*borschik:include:plugins/gallery/jquery.form.min.js*/;
 /*borschik:include:plugins/gallery/gallery.js*/;
 /*borschik:include:plugins/image_classes/image_classes.js*/
 
-$('.redactor').redactor({
-    imageUpload: '/uploader/image',
-    imageGetJson: '/uploader/images_list',
-    plugins: ['gallery', 'image_classes'],
-    lang: 'ru'
+/* global modules:false */
+
+modules.define('redactor', ['i-bem__dom', 'jquery'], function(provide, BEMDOM, $) {
+
+provide(BEMDOM.decl(this.name, {
+    onSetMod : {
+        'js' : {
+            'inited' : function() {
+                this.init();
+                window.RedactorPlugins.advanced = {
+                    init: function ()
+                    {
+                        // this.buttonRemove('gallery');
+                    }
+                };
+            }
+        }
+    },
+    init : function() {
+        var params = this.params;
+        this.domElem.redactor({
+            imageUpload: params.imageUpload || '/uploader/image',
+            imageGetJson: params.imageGetJson || '/uploader/images_list',
+            plugins: params.plugins || [], // есть плагины ['gallery', 'image_classes']
+            buttons : params.buttons || false,
+            lang: params.lang || 'ru'
+        });
+    }
+}));
+
 });
+
