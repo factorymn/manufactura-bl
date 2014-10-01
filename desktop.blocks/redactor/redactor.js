adb32db348d4ff1a4dc9182aa722c8f0bbe5dc0a
Ilya Erin <ilya@erinphoto.ru>
2014-10-01 14:25:15 +0400

    add table and video redactor plugins, fix redactor.js for new version

--- desktop.blocks/redactor/redactor.js
+++ desktop.blocks/redactor/redactor.js
@@ -10,56 +10,60 @@
 
 modules.define('redactor', ['i-bem__dom', 'jquery'], function(provide, BEMDOM, $) {
 
-provide(BEMDOM.decl(this.name, {
-    onSetMod : {
-        'js' : {
-            'inited' : function() {
-                this.init();
-                window.RedactorPlugins.advanced = {
-                    init: function ()
-                    {
-                        // this.buttonRemove('gallery');
-                    }
-                };
+    provide(BEMDOM.decl(this.name, {
+        onSetMod : {
+            'js' : {
+                'inited' : function() {
+                    this.init();
+                    window.RedactorPlugins.advanced = {
+                        init: function ()
+                        {
+                            // this.buttonRemove('gallery');
+                        }
+                    };
+                }
             }
+        },
+        init : function() {
+            var params = this.params;
+            var defaultParams = {
+                imageUpload: '/uploader/image',
+                imageGetJson: '/uploader/images_list',
+                fileUpload: '/uploader/file',
+                fileUploadCallback: function(link, json)
+                {
+                    $(link).addClass('file_upload');
+                    this.sync();
+                },
+                plugins: ['table'], // есть плагины ['gallery', 'image_classes']
+                buttons: [
+                    'formatting',
+                    'bold',
+                    'italic',
+                    'deleted',
+                    'unorderedlist',
+                    'orderedlist',
+                    'outdent',
+                    'indent',
+                    'image',
+                    'video',
+                    'file',
+                    'link',
+                    'alignment',
+                    'horizontalrule'
+                ],
+                formattingTags: ['p', 'blockquote', 'pre', 'h1', 'h2', 'h3', 'h4'],
+                lang: 'ru',
+                buttonSource: true
+            };
+
+            for (var prop in params) {
+                if (prop != 'uniqId') {
+                    defaultParams[prop] = params[prop];
+                }
+            }
+            this.domElem.redactor(defaultParams);
         }
-    },
-    init : function() {
-        var params = this.params;
-        var defaultParams = {
-            imageUpload: params.imageUpload || '/uploader/image',
-            imageGetJson: params.imageGetJson || '/uploader/images_list',
-            fileUpload: params.fileUpload || '/uploader/file',
-            fileUploadCallback: params.fileUploadCallback || function(link, json)
-            {
-                $(link).addClass('file_upload');
-                this.sync();
-            },
-            plugins: params.plugins || [], // есть плагины ['gallery', 'image_classes']
-            buttons : params.buttons || [
-                'html',
-                'formatting',
-                'bold',
-                'italic',
-                'deleted',
-                'unorderedlist',
-                'orderedlist',
-                'outdent',
-                'indent',
-                'image',
-                'video',
-                'file',
-                'table',
-                'link',
-                'alignment',
-                'horizontalrule'
-            ],
-            formattingTags : params.formattingTags || ['p', 'blockquote', 'pre', 'h1', 'h2', 'h3', 'h4'],
-            lang: params.lang || 'ru'
-        };
-        this.domElem.redactor($.extend(defaultParams, params));
-    }
-}));
+    }));
 
 });
-
