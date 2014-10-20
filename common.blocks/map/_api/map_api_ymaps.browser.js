8b8a196b4f76eea698055de71bbc95ec0dd02b81
voischev <voischev.ivan@ya.ru>
2014-10-20 07:47:51 +0000

    map_api_ymaps.browser.js отредактирован онлайн на Bitbucket

--- common.blocks/map/_api/map_api_ymaps.browser.js
+++ common.blocks/map/_api/map_api_ymaps.browser.js
@@ -1,91 +1,89 @@
 modules.define('map', ['i-bem__dom', 'jquery'], function (provide, BEMDOM, $) {
-provide(BEMDOM.decl(this.name, {
-    onSetMod : {
-        'js' : function() {
-            this.loadMapsApi();
-        }
-    },
-    /**
-     * Загрузчик API.
-     */
-    loadMapsApi: function () {
-        if (!window.ymaps) {
+    provide(BEMDOM.decl({ block : 'map', modName : 'api', modVal : 'ymaps' }, {
+        onSetMod : {
+            'js' : function() {
+                this.loadMapsApi();
+            }
+        },
+        /**
+         * Загрузчик API.
+         */
+        loadMapsApi: function () {
+            if (!window.ymaps) {
 
-            var apiScript = document.createElement('script'),
-                apiCallback = 'ymapsloaded';
+                var apiScript = document.createElement('script'),
+                    apiCallback = 'ymapsloaded';
 
-            window[apiCallback] = $.proxy(function () {
+                window[apiCallback] = $.proxy(function () {
+                    this.onAPILoaded();
+                }, this);
+
+                apiScript.src = [
+                    'http://api-maps.yandex.ru/2.1/?',
+                    '&lang=' + (this.params.lang || 'ru-RU'),
+                    '&onload=' + apiCallback
+                ].join('');
+
+                document.getElementsByTagName('head')[0].appendChild(apiScript);
+
+            } else {
                 this.onAPILoaded();
-            }, this);
+            }
+        },
+        /**
+         * Выполнится после загрузки API.
+         */
+        onAPILoaded: function () {
+            // Запускаем инициализацию карты.
+            this.initMap();
+        },
+        /**
+         * Инициализация карты.
+         */
+        initMap: function () {
+            var center = (!!this.params.center[0]) ? this.params.center : [51.661535, 39.200287],
+                zoom = this.params.zoom || 10;
 
-            apiScript.src = [
-                'http://api-maps.yandex.ru/2.1/?',
-                '&lang=' + (this.params.lang || 'ru-RU'),
-                '&onload=' + apiCallback
-            ].join('');
+            this._map = new ymaps.Map(this.domElem[0], {
+                center: center,
+                zoom: zoom,
+                behaviors: ['drag', 'dblClickZoom', 'scrollZoom'],
+                controls: []
+            });
 
-            document.getElementsByTagName('head')[0].appendChild(apiScript);
 
-        } else {
-            this.onAPILoaded();
+            // Если есть метки, то добавляем их на карту.
+            if (this.params.geoObjects && this.params.geoObjects.length > 0) {
+                this.params.geoObjects.forEach(function (item) {
+
+                    var geoObject;
+
+                    item.options = item.options || {};
+                    geoObject = new ymaps.Placemark(item.coords, item.properties, item.options);
+
+                    this._map.geoObjects.add(geoObject);
+
+                }, this);
+            }
+
+            // Центруем карту так что бы влазили все добавленные объекты
+            if (this.params.setupBoundsByGeoObjects) {
+                this._map.setBounds(this._map.geoObjects.getBounds());
+            }
+
+            // Блок поделится информацией о том, что он инициализировал карту.
+            // В данных передаём ссылку на экземпляр карты.
+            // this.trigger('map-inited', {
+            //    map: this._map
+            //});
+        },
+
+        /**
+         * @return {Map | Null} Экземпляр карты, либо null, если карта не инстанцирована.
+         */
+        getMap: function () {
+            return this._map || null;
         }
-    },
-    /**
-     * Выполнится после загрузки API.
-     */
-    onAPILoaded: function () {
-        // Запускаем инициализацию карты.
-        this.initMap();
-    },
-    /**
-     * Инициализация карты.
-     */
-    initMap: function () {
-        var center = this.params.center || [55.76, 37.64],
-            zoom = this.params.zoom || 10;
 
-        this._map = new ymaps.Map(this.domElem[0], {
-            center: center,
-            zoom: zoom,
-            behaviors: ['drag', 'dblClickZoom', 'scrollZoom'],
-            controls: ['zoomControl', 'fullscreenControl']
-        });
-
-
-        // Если есть метки, то добавляем их на карту.
-        if (this.params.geoObjects && this.params.geoObjects.length > 0) {
-            this.params.geoObjects.forEach(function (item) {
-
-                var geoObject;
-
-                item.options = item.options || {};
-                geoObject = new ymaps.Placemark(item.coords, item.properties, item.options);
-
-                this._map.geoObjects.add(geoObject);
-
-            }, this);
-        }
-
-        // Центруем карту так что бы влазили все добавленные объекты
-        if (this.params.setupBoundsByGeoObjects) {
-            this._map.setBounds(this._map.geoObjects.getBounds());
-        }
-
-        // Блок поделится информацией о том, что он инициализировал карту.
-        // В данных передаём ссылку на экземпляр карты.
-        
-        // TODO: Кажется что этот метод немного ломает IE
-        this.trigger('map-inited', {
-            map: this._map
-        });
-    },
-
-    /**
-     * @return {Map | Null} Экземпляр карты, либо null, если карта не инстанцирована.
-     */
-    getMap: function () {
-        return this._map || null;
-    }
-
-}))
+    }))
 });
